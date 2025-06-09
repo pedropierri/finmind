228154695276299492c8b2aefbfb42aa8caebea2
Pedro Pierri <pedropmp05@gmail.com>
2025-06-09 16:21:27 -0300

    add animations

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
+    //Scroll
+    const lenis = new Lenis({
+        duration: 1.7,
+        lerp: 0.07,
+        wheelMultiplier: 1,
+        touchMultiplier: 2, 
+        smoothWheel: true,   
+        smoothTouch: false, 
+    })
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            lenis.scrollTo(this.getAttribute('href'));
+        });
+    });
+
+    requestAnimationFrame(raf);
+
+    //Testimonials
     const track = document.querySelector('.carousel-track');
     
     if (!track) {
-        console.error("Elemento .carousel-track não encontrado.");
+        console.error("Elemento .carousel-track not founded.");
         return;
     }
 
@@ -25,4 +49,23 @@ document.addEventListener('DOMContentLoaded', () => {
     setInterval(moveToNextSlide, 4000);
     window.addEventListener('resize', updateCarouselPosition);
     updateCarouselPosition();
+
+    // Animations
+    const elementsToAnimate = document.querySelectorAll('.animate-on-scroll');
+
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.5
+    });
+
+    elementsToAnimate.forEach(element => {
+        observer.observe(element);
+    });
 });
